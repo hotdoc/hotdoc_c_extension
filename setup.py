5e2c3d61c1b5b42c6597d9866147907845b36a6c
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2015-12-08 09:48:37 +0100

    Initial import from hotdoc

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "hotdoc-c-extension",
+    version = "0.6",
+    keywords = "C clang hotdoc",
+    url='https://github.com/MathieuDuponchelle/hotdoc-c-extension',
+    author_email = 'mathieu.duponchelle@opencreed.com',
+    license = 'LGPL',
+    description = "An extension for hotdoc that parses C using clang",
+    author = "Mathieu Duponchelle",
+    packages = find_packages(),
+    entry_points = {'hotdoc.extensions': 'get_extension_class =
+        c_extension:get_extension_class'},
+    install_requires = [
+        'hotdoc>=0.6',
+        'clang',
+    ]
+)

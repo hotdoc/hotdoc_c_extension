cee7cb332ae16c64ec9789845f913806e354ee27
Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
2016-03-07 13:10:29 +0100

    License hotdoc's C extension to LGPLv2.1+

--- README.md
+++ README.md
@@ -65,3 +65,8 @@ Checkout the code from github, then run:
 ```
 python setup.py develop
 ```
+
+### Licensing
+
+hotdoc's C extension is licensed under the LGPL version 2.1 (or, at your option, any
+later version). See COPYING for more details.
--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2015,2016 Mathieu Duponchelle <mathieu.duponchelle@opencreed.com>
+# Copyright © 2015,2016 Collabora Ltd
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library.  If not, see <http://www.gnu.org/licenses/>.
+
 import os, subprocess
 
 from pkg_resources import parse_version as V
@@ -92,7 +110,7 @@ setup(
     keywords = "C clang hotdoc",
     url='https://github.com/hotdoc/hotdoc_c_extension',
     author_email = 'mathieu.duponchelle@opencreed.com',
-    license = 'LGPL',
+    license = 'LGPLv2.1+',
     description = "An extension for hotdoc that parses C using clang",
     author = "Mathieu Duponchelle",
     packages = find_packages(),

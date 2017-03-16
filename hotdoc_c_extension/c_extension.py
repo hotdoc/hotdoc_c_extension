970d83bba6a22dafb526a89b75591d68d7f2c188
Thibault Saunier <thibault.saunier@osg.samsung.com>
2017-03-16 01:35:46 -0300

    c_extension: Factor out a class to extract C comments in the database

--- hotdoc_c_extension/c_extension.py
+++ hotdoc_c_extension/c_extension.py
@@ -18,8 +18,6 @@
 
 import os, sys, linecache, pkgconfig, glob, subprocess
 
-import cchardet
-
 from hotdoc_c_extension.clang import cindex
 from ctypes import *
 from fnmatch import fnmatch
@@ -32,11 +30,10 @@ from hotdoc.core.comment import comment_from_tag
 from hotdoc.core.links import Link
 
 from hotdoc.parsers.gtk_doc import GtkDocParser
+from .utils.utils import CCommentExtractor
 
 from hotdoc.utils.loggable import (info as core_info, warn, Logger,
     debug as core_debug)
-
-from .c_comment_scanner.c_comment_scanner import extract_comments
 
 def ast_node_is_function_pointer (ast_node):
     if ast_node.kind == cindex.TypeKind.POINTER and \
@@ -53,10 +50,6 @@ def info(message):
 def debug(message):
     core_debug(message, domain='c-extension')
 
-
-def unicode_dammit(data):
-    encoding = cchardet.detect(data)['encoding']
-    return data.decode(encoding, errors='replace')
 
 Logger.register_warning_code('clang-diagnostic', ParsingException,
                              'c-extension')
@@ -99,7 +92,6 @@ class ClangScanner(object):
             cindex.Config.set_compatibility_check(False)
 
         self.app = app
-        self.__raw_comment_parser = GtkDocParser(project)
         self.project = project
         self.__doc_db = doc_db
         self.__all_sources = []
@@ -155,26 +147,9 @@ class ClangScanner(object):
                     self.__parse_file (fname, tu, full_scan)
 
         if not full_scan:
-            for filename in filenames:
-                with open (filename, 'rb') as f:
-                    skip_next_symbol = filename in header_guarded
-                    debug('Getting comments in %s' % filename)
-                    lines = [unicode_dammit(l) for l in f.readlines()]
-                    cs = extract_comments (''.join(lines))
-                    for c in cs:
-                        if c[3]:
-                            line = lines[c[1] - 1]
-
-                            comment = (len(line) - len(line.lstrip(' '))) * ' ' + c[0]
-                            block = self.__raw_comment_parser.parse_comment(comment,
-                                filename, c[1], c[2], self.project.include_paths)
-                            if block is not None:
-                                self.app.database.add_comment(block)
-                        elif not skip_next_symbol:
-                            if filename.endswith('.h'):
-                                self.__create_macro_from_raw_text(c, filename)
-                        else:
-                            skip_next_symbol = False
+            comment_parser = GtkDocParser(self.project)
+            CCommentExtractor(self.__doc_db, comment_parser).parse_comments(
+                filenames)
 
         return True
 
@@ -499,50 +474,6 @@ class ClangScanner(object):
             sym = self.__create_alias_symbol (node)
 
         return sym
-
-    def __create_macro_from_raw_text(self, raw, filename):
-        mcontent = raw[0].replace('\t', ' ')
-        mcontent = mcontent.split(' ', 1)[1]
-        split = mcontent.split('(', 1)
-        name = split[0]
-        if not (' ' in name or '\t' in name) and len(split) == 2:
-            args = split[1].split(')', 1)[0].split(',')
-            if args:
-                stripped_name = name.strip()
-                return self.__create_function_macro_symbol(stripped_name,
-                    filename, raw[1], raw[0])
-
-        name = mcontent.split(' ', 1)[0]
-        stripped_name = name.strip()
-        return self.__create_constant_symbol(stripped_name, filename, raw[1], raw[0])
-
-    def __create_function_macro_symbol (self, name, filename, lineno, original_text):
-        comment = self.app.database.get_comment(name)
-
-        return_value = [None]
-        if comment:
-            return_tag = comment.tags.get ('returns')
-            if return_tag:
-                return_value = [ReturnItemSymbol ()]
-
-        parameters = []
-
-        if comment:
-            for param_name in comment.params:
-                parameter = ParameterSymbol (argname=param_name)
-                parameters.append (parameter)
-
-        sym = self.__doc_db.get_or_create_symbol(FunctionMacroSymbol, return_value=return_value,
-                parameters=parameters, original_text=original_text,
-                display_name=name,
-                filename=filename, lineno=lineno)
-        return sym
-
-    def __create_constant_symbol (self, name, filename, lineno, original_text):
-        return self.__doc_db.get_or_create_symbol(ConstantSymbol,
-                original_text=original_text,
-                display_name=name, filename=filename,
-                lineno=lineno)
 
     def __create_function_symbol (self, node):
         parameters = []
